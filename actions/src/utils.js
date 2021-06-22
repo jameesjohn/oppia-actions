b54bf74208dbd44ea441e970a6c49f916f7ab38e
James James <jamesjay4199@gmail.com>
2021-06-22 15:23:00 +0100

    minor updates

--- actions/src/utils.js
+++ actions/src/utils.js
@@ -1,3 +1,5 @@
+const { context } = require('@actions/github');
+
 /**
  * This function pings and assigns all pending reviewers to a pull request.
  * @param {import('@octokit/rest').Octokit} octokit
@@ -5,22 +7,20 @@
  * @param {string[]} assignees
  * @param {string} comment
  */
- const pingAndAssignUsers = async (octokit, pullRequest, assignees, comment) => {
-  await octokit.issues.createComment(
-    context.repo({
-      issue_number: pullRequest.number,
-      body: comment,
-    })
-  );
+const pingAndAssignUsers = async (octokit, pullRequest, assignees, comment) => {
+  await octokit.issues.createComment({
+    issue_number: pullRequest.number,
+    body: comment,
+    ...context.repo,
+  });
 
-  await octokit.issues.addAssignees(
-    context.repo({
-      issue_number: pullRequest.number,
-      assignees: assignees,
-    })
-  );
+  await octokit.issues.addAssignees({
+    issue_number: pullRequest.number,
+    assignees: assignees,
+    ...context.repo,
+  });
 };
 
 module.exports = {
-  pingAndAssignUsers
+  pingAndAssignUsers,
 };
